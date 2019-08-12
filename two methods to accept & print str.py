2574d51e0579a27f04698c5df9f73432f596845a
Koushika-BL <48148438+Koushika-BL@users.noreply.github.com>
2019-08-12 21:49:51 +0530

    Create two methods to accept & print str.py

---
+++ two methods to accept & print str.py
@@ -0,0 +1,13 @@
+class one():
+    def __init__(self):
+        self.a=""
+    def get_string(self):
+        self.a=input()
+    def print_string(self):
+        self.b=self.a.upper()
+        print(self.b)
+ob1=one()
+ob1.get_string()
+ob1.print_string()
+        
+        
